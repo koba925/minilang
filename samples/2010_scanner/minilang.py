43e0dcd8c5b1627555fe84b5bf386c9a306278c2
Kobayashi Takahiro <koba925@gmail.com>
2024-06-16 09:22:09 +0000

    サンプル：スキャナを分離する

---
+++ samples/2010_scanner/minilang.py
@@ -0,0 +1,52 @@
+class Scanner:
+    def __init__(self, source) -> None:
+        self._source = source
+        self._current_position = 0
+
+    def next_token(self):
+        while self._current_char().isspace(): self._current_position += 1
+
+        start = self._current_position
+        match self._current_char():
+            case "$EOF": return "$EOF"
+            case c if c.isalpha():
+                while self._current_char().isalnum() or self._current_char() == "_":
+                    self._current_position += 1
+                return self._source[start:self._current_position]
+            case c if c.isnumeric():
+                while self._current_char().isnumeric():
+                    self._current_position += 1
+                return int(self._source[start:self._current_position])
+            case _:
+                self._current_position += 1
+                return self._source[start:self._current_position]
+
+    def _current_char(self):
+        if self._current_position < len(self._source):
+            return self._source[self._current_position]
+        else:
+            return "$EOF"
+
+def interpret(source):
+    scanner = Scanner(source)
+
+    while (command := scanner.next_token()) != "$EOF":
+        assert command == "print", f"`print` expected, found `{command}`."
+
+        number = scanner.next_token()
+        assert isinstance(number, int), f"Number expected, found `{number}`."
+
+        semicolon = scanner.next_token()
+        assert semicolon == ";", f"Semicolon expected, found `{semicolon}`."
+
+        print(number)
+
+import sys
+
+while True:
+    print("Input source and enter Ctrl+D:")
+    if (source := sys.stdin.read()) == "": break
+
+    print("Output:")
+    try: interpret(source)
+    except AssertionError as e: print("Error: ", e)
