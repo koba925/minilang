8e9bd65b87dea6916fefd0108f11223da5511d1e
Kobayashi Takahiro <koba925@gmail.com>
2024-06-16 09:00:29 +0000

    Interpretクラスを関数に変更

--- samples/1020_my_implementation/minilang.py
+++ samples/1020_my_implementation/minilang.py
@@ -1,40 +1,31 @@
-class Interpreter:
-    def __init__(self, source) -> None:
-        self._source = source
-        self._current_position = 0
+def interpret(source):
+    current_position = 0
 
-    def interpret(self):
-        while self._current_char() != "$EOF":
-            while self._current_char().isspace(): self._current_position += 1
+    while current_position < len(source):
+        while source[current_position].isspace(): current_position += 1
 
-            start = self._current_position
-            while  self._current_char().isalpha(): self._current_position += 1
-            command = self._source[start:self._current_position]
-            assert command == "print", f"`print` expected."
+        start = current_position
+        while  source[current_position].isalpha(): current_position += 1
+        command = source[start:current_position]
+        assert command == "print", f"`print` expected."
 
-            while self._current_char().isspace(): self._current_position += 1
+        while source[current_position].isspace(): current_position += 1
 
-            start = self._current_position
-            while  self._current_char().isnumeric():
-                self._current_position += 1
-            assert self._source[start:self._current_position].isnumeric(), f"Number expected."
-            number = int(self._source[start:self._current_position])
+        start = current_position
+        while  source[current_position].isnumeric():
+            current_position += 1
+        assert source[start:current_position].isnumeric(), f"Number expected."
+        number = int(source[start:current_position])
 
-            while self._current_char().isspace(): self._current_position += 1
+        while source[current_position].isspace(): current_position += 1
 
-            start = self._current_position
-            self._current_position += 1
-            assert self._source[start:self._current_position] == ";", f"Semicolon expected."
+        start = current_position
+        current_position += 1
+        assert source[start:current_position] == ";", f"Semicolon expected."
 
-            while self._current_char().isspace(): self._current_position += 1
+        while source[current_position].isspace(): current_position += 1
 
-            print(number)
-
-    def _current_char(self):
-        if self._current_position < len(self._source):
-            return self._source[self._current_position]
-        else:
-            return "$EOF"
+        print(number)
 
 import sys
 
@@ -43,5 +34,5 @@ while True:
     if (source := sys.stdin.read()) == "": break
 
     print("Output:")
-    try: Interpreter(source).interpret()
+    try: interpret(source)
     except AssertionError as e: print("Error: ", e)
