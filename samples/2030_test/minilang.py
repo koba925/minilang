6c36f32c511e3724020a854a941593dc5ce75775
Kobayashi Takahiro <koba925@gmail.com>
2024-06-17 12:34:54 +0000

    サンプル：テストを実装する

--- samples/2030_test/minilang.py
+++ samples/2030_test/minilang.py
@@ -27,7 +27,6 @@ class Scanner:
         else:
             return "$EOF"
 
-
 class Parser:
     def __init__(self, source):
         self.scanner = Scanner(source)
@@ -58,6 +57,12 @@ class Parser:
         return self._current_token
 
 class Evaluator:
+    def __init__(self):
+        self._output = []
+
+    def clear_output(self): self._output = []
+    def output(self): return self._output
+
     def eval_program(self, program):
         match program:
             case ["program", *statements]:
@@ -67,20 +72,22 @@ class Evaluator:
 
     def _eval_statement(self, statement):
         match statement:
-            case ["print", val]: print(val)
+            case ["print", val]: self._output.append(val)
             case unexpected: assert False, f"Internal Error at `{unexpected}`."
 
-import sys
+if __name__ == "__main__":
+    import sys
 
-evaluator = Evaluator()
-while True:
-    print("Input source and enter Ctrl+D:")
-    if (source := sys.stdin.read()) == "": break
+    evaluator = Evaluator()
+    while True:
+        print("Input source and enter Ctrl+D:")
+        if (source := sys.stdin.read()) == "": break
 
-    print("Output:")
-    try:
-        ast = Parser(source).parse_program()
-        print(ast)
-        evaluator.eval_program(ast)
-    except AssertionError as e:
-        print("Error:", e)
+        print("Output:")
+        try:
+            ast = Parser(source).parse_program()
+            print(ast)
+            evaluator.eval_program(ast)
+            print(*evaluator.output(), sep="\n")
+        except AssertionError as e:
+            print("Error:", e)
